6791107ca84a686b94db2ab52076a40dae045dbb
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2023-11-13 00:18:11 +0900

    adding delete invoice and fix build error

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,26 +1,30 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data'; 
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
         fetchCustomers(),
-      ]);
-  return (
-    <main>
-      <Breadcrumbs
-        breadcrumbs={[
-          { label: 'Invoices', href: '/dashboard/invoices' },
-          {
-            label: 'Edit Invoice',
-            href: `/dashboard/invoices/${id}/edit`,
-            active: true,
-          },
-        ]}
-      />
-      <Form invoice={invoice} customers={customers} />
-    </main>
-  );
+    ]);
+    if (invoice !== undefined) {
+        return (
+            <main>
+                <Breadcrumbs
+                    breadcrumbs={[
+                        { label: 'Invoices', href: '/dashboard/invoices' },
+                        {
+                            label: 'Edit Invoice',
+                            href: `/dashboard/invoices/${id}/edit`,
+                            active: true,
+                        },
+                    ]}
+                />
+                <Form invoice={invoice} customers={customers} />
+            </main>
+        );
+    } else {
+        return (<div>Something seems to be wrong ...</div>)
+    }
 }
